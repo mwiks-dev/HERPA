473480151e4614c655e91974ce39fc3dfb91ace0
Maryann Mwikali <mwikali119@gmail.com>
2022-10-16 23:22:39 +0300

    Write event class in models

--- Backend/Herpa_Project/auth_app/models.py
+++ Backend/Herpa_Project/auth_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Event(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 2000)
+    tel_number = models.IntegerField(null=True)
+    email = models.EmailField()
+
+    def save_event(self):
+        self.save
+    
+    def update_event(self):
+        self.save
+
+    def delete_event(self):
+        self.delete
+
+    def __str__(self):
+        return self.name
